9ec62abdc3fcfb0cb3490c9474e9f7b1526bf258
Manuh08 <manuelasoarespacheco11@gmail.com>
2024-10-29 00:25:50 -0400

    Continuação

--- databasepg.js
+++ databasepg.js
@@ -1,3 +1,24 @@
 const {Client} = require('pg')
 
-const client = 
+const client = new Client({
+    host: "localhost",
+    user: "postgres",
+    port: 5432,
+    password: "rootUser",
+    database: "postgres"
+})
+$sql = "SELECT * FROM pessoas ORDER BY id DESC";
+
+$result= $conexao->querry($sql);
+client.connect();
+
+client.query(`Select * from users`, (err, res)=>{
+    if(!err){
+        console.log(res.rows);
+    } else {
+        console.log(err.message);
+    }
+    client.end;
+})
+
+
